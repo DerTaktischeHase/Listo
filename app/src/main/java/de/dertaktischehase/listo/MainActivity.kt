8d9fd6a68a6948363645f22335d2a0d18315e908
DerTaktischeHase <timo.hartstang@mail.de>
2025-10-14 02:17:21 +0200

    Initial commit

---
+++ app/src/main/java/de/dertaktischehase/listo/MainActivity.kt
@@ -0,0 +1,13 @@
+package de.dertaktischehase.listo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+    }
+}
